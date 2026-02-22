8c525e451c0d07ec67f453a799864fbedb3751bb
jhaddixcodes <jameshaddix1110@gmail.com>
2026-02-22 00:57:19 -0800

    Add global variable to `ping`, change concatenation to f-strings

--- frontend/httpRequest.py
+++ frontend/httpRequest.py
@@ -2,26 +2,21 @@ import requests
 
 URL = "http://10.109.206.111:2026"
 
-
 def ping():
-    return requests.get("http://10.109.206.111:2026/ping").json()["message"] == "pong"
-
+    return requests.get(f"{URL}/ping").json()["message"] == "pong"
 
 def player2Connect(gameCode: str):
     payload = {"id": gameCode}
-    return requests.get(URL + "/join", params=payload).json()
-
+    return requests.get(f"{URL}/join", params=payload).json()
 
 def createRoom(gameCode: str):
     payload = {"id": gameCode}
-    return requests.get(URL + "/create", params=payload).json()
-
+    return requests.get(f"{URL}/create", params=payload).json()
 
 def getGame(gameCode: str, player: str):
     payload = {"id": gameCode, "player": player}
-    return requests.get(URL + "/getGame", params=payload).json()
-
+    return requests.get(f"{URL}/getGame", params=payload).json()
 
 def submitAnswer(gameCode: str, player: str, answer: str):
     payload = {"id": gameCode, "player": player, "answer": answer}
-    return requests.get(URL + "/submit", params=payload).json()
+    return requests.get(f"{URL}/submit", params=payload).json()
