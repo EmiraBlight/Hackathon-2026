8626da8cffac3602cdf3915aef1383d9f6cf423b
S1RY1 <wyatt@pixelhammer.net>
2026-02-21 18:11:01 -0800

    made things

---
+++ frontend/frontend.py
@@ -0,0 +1,73 @@
+from tkinter import *
+from tkinter import ttk
+
+
+class Frontend:
+    def raiseFrame(self, frame: ttk.Frame):
+        return lambda: frame.tkraise()
+    def __init__(self, root):
+        # establish root
+        self.root = root
+        root.title("Two AIs and a Human")
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        # establish mainframe
+        mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        mainframe.columnconfigure(0, weight=1)
+        mainframe.rowconfigure(0, weight=1)
+
+
+
+        ttk.Style().configure("Main.TButton", font=("Consolas", 50))
+        ttk.Style().configure("Main.TLabel", font=("Consolas", 50))
+
+
+
+        # write answer menu
+        self.writeAnswerFrame = ttk.Frame(mainframe)
+
+        # write questions menu
+        self.writeQuestionsFrame = ttk.Frame(mainframe)
+
+        # enter code menu
+        self.enterCodeFrame = ttk.Frame(mainframe)
+
+        # game code menu
+        self.waitingFrame = ttk.Frame(mainframe)
+        
+        ttk.Label(self.waitingFrame, text="Waiting for Second Player...", padding=10, style="Main.TLabel").grid(row=0, sticky="N")
+        ttk.Label(self.waitingFrame, text="Code", padding=10, style="Main.TLabel").grid(row=1)
+
+
+
+        self.waitingFrame.columnconfigure(0, weight=1)
+        self.waitingFrame.rowconfigure(1, weight=1)
+
+        # mainmenu frame ### DO NOT MOVE THIS "DO NOT LEAVE YET"
+        self.mainMenu = ttk.Frame(mainframe)
+
+        ttk.Label(self.mainMenu, text="Main Menu", padding=10, style="Main.TLabel").grid(row=0)
+        ttk.Button(self.mainMenu, text="Create Game", padding=10, style="Main.TButton", command=self.raiseFrame(self.waitingFrame)).grid(row=1)
+        ttk.Button(self.mainMenu, text="Join Game", padding=10, style="Main.TButton").grid(row=2) # TODO: add a function to validate join code with a generated game code on backend
+
+
+        self.mainMenu.columnconfigure(0, weight=1)
+        self.mainMenu.rowconfigure((0, 1, 2), weight=1)
+
+
+        for child in mainframe.winfo_children(): 
+            child.grid_configure(sticky=(N, W, E, S), row=0, column=0)
+        self.raiseFrame(self.mainMenu)
+        
+
+
+
+
+
+root = Tk()
+Frontend(root)
+root.state('zoomed')
+
+root.mainloop()
