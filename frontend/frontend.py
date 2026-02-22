754c5e597d2f4a667edb0b533b0786a3671be13e
jhaddixcodes <jameshaddix1110@gmail.com>
2026-02-21 18:17:27 -0800

    Added game code generation method

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,10 +1,15 @@
 from tkinter import *
 from tkinter import ttk
-
+from random import randint
 
 class Frontend:
+
+    def generate_game_code(self):
+        return randint(10000000, 99999999)
+
     def raiseFrame(self, frame: ttk.Frame):
         return lambda: frame.tkraise()
+
     def __init__(self, root):
         # establish root
         self.root = root
@@ -18,12 +23,8 @@ class Frontend:
         mainframe.columnconfigure(0, weight=1)
         mainframe.rowconfigure(0, weight=1)
 
-
-
         ttk.Style().configure("Main.TButton", font=("Consolas", 50))
         ttk.Style().configure("Main.TLabel", font=("Consolas", 50))
-
-
 
         # write answer menu
         self.writeAnswerFrame = ttk.Frame(mainframe)
@@ -40,8 +41,6 @@ class Frontend:
         ttk.Label(self.waitingFrame, text="Waiting for Second Player...", padding=10, style="Main.TLabel").grid(row=0, sticky="N")
         ttk.Label(self.waitingFrame, text="Code", padding=10, style="Main.TLabel").grid(row=1)
 
-
-
         self.waitingFrame.columnconfigure(0, weight=1)
         self.waitingFrame.rowconfigure(1, weight=1)
 
@@ -52,19 +51,12 @@ class Frontend:
         ttk.Button(self.mainMenu, text="Create Game", padding=10, style="Main.TButton", command=self.raiseFrame(self.waitingFrame)).grid(row=1)
         ttk.Button(self.mainMenu, text="Join Game", padding=10, style="Main.TButton").grid(row=2) # TODO: add a function to validate join code with a generated game code on backend
 
-
         self.mainMenu.columnconfigure(0, weight=1)
         self.mainMenu.rowconfigure((0, 1, 2), weight=1)
-
 
         for child in mainframe.winfo_children(): 
             child.grid_configure(sticky=(N, W, E, S), row=0, column=0)
         self.raiseFrame(self.mainMenu)
-        
-
-
-
-
 
 root = Tk()
 Frontend(root)
