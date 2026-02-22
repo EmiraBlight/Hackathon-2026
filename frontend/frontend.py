9f24374b6d33063c49775298a68f6be52a39bb92
Zylefer <connorcolemangamer@gmail.com>
2026-02-22 08:42:32 -0800

    FINAL STUFF???

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -112,6 +112,7 @@ class FrontEnd:
         # guess answer menu
         def drawGuessAnswerFrame():
             self.guessAnswerFrame = ttk.Frame(mainframe)
+            self.guessAnswerFrame.grid_configure(sticky=(N, S, E, W), row=0, column=0)
 
             ttk.Label(
                 self.guessAnswerFrame,
@@ -199,7 +200,7 @@ class FrontEnd:
         # write answer menu
         def drawWriteAnswerFrame():
             self.writeAnswerFrame = ttk.Frame(mainframe)
-            self.writeAnswerFrame.grid_configure(sticky=(S, E, W), row=0, column=0)
+            self.writeAnswerFrame.grid_configure(sticky=(N, S, E, W), row=0, column=0)
 
             ttk.Label(
                 self.writeAnswerFrame, text="Write Answer", padding=10, style="Main.TLabel"
